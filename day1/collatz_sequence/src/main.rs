6137ec614a923d3b699e5759bcea77fa666038e7
Saad Abdul Majid <saadmajidnadeem1@gmail.com>
2025-05-24 15:19:45 +0500

    Make use of if expression
    Signed-off-by: Saad Abdul Majid <saadmajidnadeem1@gmail.com>

--- day1/collatz_sequence/src/main.rs
+++ day1/collatz_sequence/src/main.rs
@@ -3,10 +3,10 @@ fn collatz_length(mut n: i32) -> u32 {
     let mut length = 1;
     while n != 1 {
         length += 1;
-        if n % 2 == 0 {
-            n /= 2;
+        n = if n % 2 == 0 {
+            n / 2
         } else {
-            n = (3 * n) + 1;
+            (3 * n) + 1
         }
     }
     length
