a7c3d879d1360d82cc9399f471ef2340761e0b6e
Saad Abdul Majid <saadmajidnadeem1@gmail.com>
2025-05-24 12:49:36 +0500

    fibonacci exercise
    Signed-off-by: Saad Abdul Majid <saadmajidnadeem1@gmail.com>

---
+++ day1/fibonacci/src/main.rs
@@ -0,0 +1,17 @@
+fn fib(n: u32) -> u32 {
+    if n == 0 {
+        return 0; // it should panic here instead
+    } else if n == 1 {
+        return 0;
+    }
+    if n == 2 {
+        return 1;
+    } else {
+        return fib(n -1) + fib(n - 2);
+    }
+}
+
+fn main() {
+    let n = 7;
+    println!("fib({n}) = {}", fib(n));
+}
