6710c94bbfda1e5013aa78a2da2215f313e3afa6
Saad Abdul Majid <saadmajidnadeem1@gmail.com>
2025-05-25 16:56:56 +0500

    tranpose matrix exercise
    Signed-off-by: Saad Abdul Majid <saadmajidnadeem1@gmail.com>

---
+++ day1/transpose_matrix/src/main.rs
@@ -0,0 +1,21 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut transposed = [[0; 3]; 3];
+    for i in 0..matrix.len() {
+        for j in 0..matrix[i].len() {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+    transposed
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    dbg!(matrix);
+    let transposed = transpose(matrix);
+    dbg!(transposed);
+}
